d6143b0d5eba2eee4bd68bb04b4fd7dc6a3f88e0
BadKitty-png <teamenduro1dh@gmail.com>
2026-09-24 15:35:46 -0600

    Recursividad Imprimiendo varias veces Saludo

---
+++ datos/unidad1/recursividad/Recursividad.java
@@ -0,0 +1,18 @@
+package datos.unidad1.recursividad;
+
+public class Recursividad {
+		
+
+	public static void saludo(int total) {//Condicion base
+		if(total <= 0)
+			return;
+		else{//Condicion recursiva o repetitiva
+			System.out.println("Hola Putito");
+			saludo(total - 1);
+		}
+	}
+
+	public static void main(String[] a){
+		saludo(10);
+	}
+}
