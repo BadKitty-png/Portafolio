508e3ccf20029e9f423b82571b5bbf1672daf0ec
BadKitty-png <1225100330.jcm@gmail.com>
2026-09-25 14:44:32 -0600

    Contador Funcion Recurisiva recorre y suma arreglos

--- datos/unidad1/recursividad/Recursividad.java
+++ datos/unidad1/recursividad/Recursividad.java
@@ -32,6 +32,47 @@ public class Recursividad {
 
     public static void main(String[] args) {
         //saludo("Jan", 100); 
-	cuentaRegresiva(100);
+		//cuentaRegresiva(100);
+		int[] datos = {10, 4, 5, 6, 7, 8, 4};
+		int resultado = suma(datos, datos.length - 1);
+        	System.out.println("La suma es: " + resultado);
     }
+	/*Suma con recursiva, */
+		
+	public static int suma(int[] datos, int longitud){
+		if (longitud < 0) {
+            return 0;
+        }
+		
+		return datos[longitud] + suma(datos, longitud - 1);
+
+		
+	}
 }
+/*public class SumaRecursiva {
+
+    public static void main(String[] args) {
+        int[] datos = {10, 4, 5, 6, 7, 8, 4};
+        
+        // Pasamos el arreglo y el índice del último elemento (7 elementos -> índice 6)
+        int resultado = suma(datos, datos.length - 1);
+        
+        System.out.println("La suma es: " + resultado);
+    }
+
+    public static int suma(int[] datos, int indice) {
+        // Caso base: cuando hemos procesado todo el arreglo (índice menor a 0)
+        if (indice < 0) {
+            return 0;
+        }
+        
+        // Paso recursivo: elemento actual + suma de los elementos restantes
+        return datos[indice] + suma(datos, indice - 1);
+    }
+}*/
+
+
+
+
+
+
