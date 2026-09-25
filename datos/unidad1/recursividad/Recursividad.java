e3b73aa698b73b5a0447735cbf9ed782df56d3c8
BadKitty-png <teamenduro1dh@gmail.com>
2026-09-25 13:54:25 -0600

    Contador Funcion Recurisiva

--- datos/unidad1/recursividad/Recursividad.java
+++ datos/unidad1/recursividad/Recursividad.java
@@ -3,7 +3,8 @@ package datos.unidad1.recursividad;
 public class Recursividad {
 
      public static void saludo(String name, int total) {
-         if (total <= 0) {
+        
+       if (total <= 0) {
             return;
        }
 
@@ -14,7 +15,17 @@ public class Recursividad {
         saludo(name, total - 1);
     }
 
+	public static void cuentaRegresiva(int n) {
+		if(n < 1){
+			return;
+		}else{
+			System.out.println(n+" ");
+			cuentaRegresiva(n-1);
+		}
+	}
+
     public static void main(String[] args) {
-        saludo("Jan", 100); 
+        //saludo("Jan", 100); 
+	cuentaRegresiva(100);
     }
 }
